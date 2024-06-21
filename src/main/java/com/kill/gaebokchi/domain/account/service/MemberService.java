3b4cf3cf65d701f2d4f44073c22deb6b00d1d1e1
wiishj <inging007@naver.com>
2024-06-21 18:27:10 +0900

    update : logout

--- src/main/java/com/kill/gaebokchi/domain/account/service/MemberService.java
+++ src/main/java/com/kill/gaebokchi/domain/account/service/MemberService.java
@@ -4,11 +4,18 @@ import com.kill.gaebokchi.domain.account.dto.request.SignUpRequestDTO;
 import com.kill.gaebokchi.domain.account.entity.Member;
 import com.kill.gaebokchi.domain.account.entity.Role;
 import com.kill.gaebokchi.domain.account.repository.MemberRepository;
+import com.kill.gaebokchi.domain.bogu.entity.DefaultBogu;
+import com.kill.gaebokchi.domain.bogu.entity.EvolvedBogu;
+import com.kill.gaebokchi.domain.bogu.repository.DefaultBoguRepository;
+import com.kill.gaebokchi.domain.bogu.repository.EvolvedBoguRepository;
+import com.kill.gaebokchi.domain.bogu.service.DefaultBoguService;
 import com.kill.gaebokchi.global.error.BadRequestException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static com.kill.gaebokchi.global.error.ExceptionCode.*;
 
@@ -18,6 +25,8 @@ import static com.kill.gaebokchi.global.error.ExceptionCode.*;
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final EvolvedBoguRepository evolvedBoguRepository;
+    private final DefaultBoguRepository defaultBoguRepository;
 
     public Member findMemberByEmail(String email){
         return memberRepository.findByEmail(email).orElseThrow(()-> new BadRequestException(NOT_FOUND_MEMBER_EMAIL));
@@ -38,7 +47,16 @@ public class MemberService {
     }
     @Transactional
     public void deleteMember(Member member){
+        List<EvolvedBogu> evolvedBoguList = evolvedBoguRepository.findByHost(member);
+        for(EvolvedBogu evolvedBogu : evolvedBoguList){
+            evolvedBoguRepository.delete(evolvedBogu);
+        }
+        List<DefaultBogu> defaultBoguList = defaultBoguRepository.findByHost(member);
+        for (DefaultBogu defaultBogu : defaultBoguList) {
+            defaultBoguRepository.delete(defaultBogu);
+        }
         memberRepository.delete(member);
+
     }
 
     @Transactional
